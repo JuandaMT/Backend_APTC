9aa2bb39b1da6bb9e1da9e6278f0a46b636f254f
JuandaMT <juandamt@outlook.es>
2023-11-03 12:01:48 +0100

    terminado middleware de errores

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+const { handleTypeError }= require('./middleware/errors');
 const { dbConnection } = require("./config/config");
+
 app.use(express.json());
+dbConnection();
 
 app.use('/products', require('./routes/products'));
 app.use('/users', require('./routes/users.js'));
 app.use('/orders', require('./routes/orders.js'));
 
-dbConnection();
+app.use(handleTypeError)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
